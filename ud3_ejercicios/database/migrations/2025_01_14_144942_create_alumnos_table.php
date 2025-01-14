04b7f2e6e42ff40c3add8685528817d8befe83b8
ilyas <elbekkaliilyas2003@gmail.com>
2025-01-14 16:12:01 +0100

    ultimos cambios

---
+++ ud3_ejercicios/database/migrations/2025_01_14_144942_create_alumnos_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlumnosTable extends Migration
+{
+    public function up()
+    {
+        if (!Schema::hasTable('alumnos')) {
+            Schema::create('alumnos', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre');
+                $table->string('email')->unique();
+                $table->timestamps();
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+}
